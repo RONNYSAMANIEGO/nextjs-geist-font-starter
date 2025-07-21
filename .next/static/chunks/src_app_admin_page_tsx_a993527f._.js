715cadc80553230617e436ee4142db816d231b4c
Ronny Samaniego <140879279+RONNYSAMANIEGO@users.noreply.github.com>
2025-07-21 11:47:35 -0500

    Add .next/static/chunks/src_app_admin_page_tsx_a993527f._.js 1753116455262

---
+++ .next/static/chunks/src_app_admin_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_28641a6a._.js",
+  "static/chunks/node_modules_641dda09._.js"
+],
+    source: "dynamic"
+});
